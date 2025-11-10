882bf70bd0c8ce7694300efede1bede90b3d4f71
jztchl <kmvishnu625@gmail.com>
2025-11-10 20:44:43 +0530

    centeralized secerts to config now using pydantic_settings

--- db/core.py
+++ db/core.py
@@ -1,14 +1,12 @@
-import os
 from typing import Annotated
 
-from dotenv import load_dotenv
 from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
-load_dotenv()
+from config import settings
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
